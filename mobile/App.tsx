bd2fc3f02011bf5315968898116412f9cc9bfcfb
Christopher Jones <tkcwebdev@gmail.com>
2022-09-09 09:01:11 -0700

    2: Made another update to check hot reload. I think we are ready

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#8f35cf',
+		backgroundColor: '#78cf35',
 		color: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
