5be51fd14711c77d73b85349e78eec0bd86e5054
Christopher Jones <tkcwebdev@gmail.com>
2022-09-09 09:27:02 -0700

    2: Install native base

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NativeBaseProvider, View, Text } from 'native-base';
+import theme from './src/theme';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<NativeBaseProvider theme={theme()}>
+			<View flex={1} alignItems={'flex-end'} style={styles.container}>
+				<Text
+					color='#FFF'
+					lineHeight={24}
+					fontSize={24}
+					textAlign='center'
+					fontWeight='bold'
+					textTransform={'uppercase'}
+				>
+					Open up App.js to start working on your app!
+				</Text>
+				<StatusBar style='auto' />
+			</View>
+		</NativeBaseProvider>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#78cf35',
 		color: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
