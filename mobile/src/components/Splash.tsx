33604b37a70661fda6304457486094891b5b8e77
Christopher Jones <tkcwebdev@gmail.com>
2022-09-12 18:37:12 -0700

    5: Replace Splash screen with the one designed, placeholder

--- mobile/src/components/Splash.tsx
+++ mobile/src/components/Splash.tsx
@@ -1,16 +1,14 @@
 import { Image } from 'native-base';
 import { StyleSheet } from 'react-native';
-import splash from '../../assets/splash3.png';
 
 const styles = StyleSheet.create({
 	Splash: {
-		// backgroundColor: '#F50f09',
 		flex: 1,
 	},
 });
 
 const Splash = () => {
-	const img = require('../../assets/splash3.png');
+	const img = require('../../assets/splash.png');
 	return <Image source={img} alt='Splash Screen' style={styles.Splash} />;
 };
 
