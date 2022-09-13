ecb11e9d05900c0a92073faf4b7e8085adb0caf8
Christopher Jones <tkcwebdev@gmail.com>
2022-09-12 18:37:39 -0700

    5: Remove custom splash loading animation and used native one

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,15 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import theme from './src/theme';
 import Stack from './src/navigation';
 import Home from './src/screens/Home';
-import Splash from './src/components/Splash';
 
 const App = () => {
-	const [loading, setLoading] = useState<boolean>(true);
-
-	useEffect(() => {
-		setTimeout(() => setLoading(false), 100);
-	}, []);
-
 	const globalScreenOptions: any = {
 		/** TODO: Delete once top nav is complete */
 		headerShown: false,
@@ -20,23 +13,16 @@ const App = () => {
 	};
 	return (
 		<NativeBaseProvider theme={theme()}>
-			{!loading ? (
-				<>
-					<StatusBar animated={true} barStyle='light-content' />
-					<Splash />
-				</>
-			) : (
-				<NavigationContainer>
-					<StatusBar animated={true} barStyle='light-content' />
-					<Stack.Navigator initialRouteName='Home'>
-						<Stack.Screen
-							name='Home'
-							component={Home}
-							options={globalScreenOptions}
-						/>
-					</Stack.Navigator>
-				</NavigationContainer>
-			)}
+			<NavigationContainer>
+				<StatusBar animated={true} barStyle='light-content' />
+				<Stack.Navigator initialRouteName='Home'>
+					<Stack.Screen
+						name='Home'
+						component={Home}
+						options={globalScreenOptions}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
 		</NativeBaseProvider>
 	);
 };
