2ca2cce3225333afb2c78573a30165410001c1ca
Christopher Jones <tkcwebdev@gmail.com>
2022-09-19 21:39:30 -0700

    21: Use new screen templates

--- mobile/src/screens/AccountSettings.tsx
+++ mobile/src/screens/AccountSettings.tsx
@@ -1,44 +1,23 @@
-import { Text, View } from 'native-base';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { useFonts } from 'expo-font';
-import Header from '../components/Header';
+import { Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import TemplateMain from '../templates/TemplateMain';
+
 const AccountSettings = ({ navigation }) => {
-	const [fontsLoaded] = useFonts({
-		Norwester: require('../../assets/fonts/norwester.otf'),
-	});
-	if (!fontsLoaded) {
-		return null;
-	}
 	return (
-		<SafeAreaView style={styles.page}>
-			<View alignItems={'flex-end'} style={styles.AccountSettings}>
-				<Header navigation={navigation} />
-				<Text
-					fontFamily='Norwester'
-					color='#FFF'
-					lineHeight={24}
-					fontSize={24}
-					textAlign='center'
-					fontWeight='bold'
-					textTransform={'uppercase'}
-				>
-					Account Settings
-				</Text>
-			</View>
-		</SafeAreaView>
+		<TemplateMain
+			style={styles.AccountSettings}
+			title='Account Settings'
+			navigation={navigation}
+		>
+			<Text>Account Settings content goes here</Text>
+		</TemplateMain>
 	);
 };
 
 export default AccountSettings;
 
 const styles = StyleSheet.create({
-	page: {
-		flex: 1,
-		backgroundColor: '#252525',
-	},
 	AccountSettings: {
-		backgroundColor: 'red',
-		flex: 1,
-		alignItems: 'center',
+		backgroundColor: 'orange',
 	},
 });
