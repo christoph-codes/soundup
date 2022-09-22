8bfd20cfea590f87fa80068a835391ab34fc3b6d
Christopher Jones <tkcwebdev@gmail.com>
2022-09-22 03:10:37 -0700

    26: Test the new article component

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,20 @@
 import { Text } from 'native-base';
 import { StyleSheet } from 'react-native';
+import NewsArticle, { IArticleContent } from '../components/NewsArticle';
 import TemplateMain from '../templates/TemplateMain';
 
 const Home = ({ navigation }) => {
+	const testArticle: IArticleContent = {
+		title: 'Test article',
+		description:
+			'This is a test description that will be displayed below the title',
+		postedDate: Date.now(),
+		link: 'Home',
+		imgUrl: 'https://pbs.twimg.com/profile_images/1379078909631717378/xqOL26hn_400x400.jpg',
+	};
 	return (
 		<TemplateMain style={styles.Home} navigation={navigation} title='Home'>
+			<NewsArticle article={testArticle} navigation={navigation} />
 			<Text>Home Content Goes Here</Text>
 		</TemplateMain>
 	);
