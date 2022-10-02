e57370e8874c5a86d438a677a3f2a752a3060934
Christopher Jones <tkcwebdev@gmail.com>
2022-10-02 11:38:04 -0700

    23: Template Main: Add carousel component to work on dots. WIP

--- mobile/src/templates/TemplateMain.tsx
+++ mobile/src/templates/TemplateMain.tsx
@@ -1,11 +1,9 @@
-import { Image, ScrollView, Text, View } from 'native-base';
-import { Dimensions, StyleSheet, ViewStyle } from 'react-native';
+import { ScrollView, Text, View } from 'native-base';
+import { StyleSheet, ViewStyle } from 'react-native';
 import { useFonts } from 'expo-font';
 import { NavigationProp } from '@react-navigation/native';
-import Carousel from 'react-native-reanimated-carousel';
+import Carousel from '../components/Carousel';
 import testImage from '../../assets/sliders/testImage.png';
-import { useRef, useState } from 'react';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export interface TemplateMainProps {
 	navigation: NavigationProp<any>;
@@ -22,58 +20,15 @@ const TemplateMain = ({
 	title,
 	style,
 }: TemplateMainProps) => {
-	const width = Dimensions.get('window').width;
-	const carouselRef = useRef(null);
-	const [activeSlide, setActiveSlide] = useState(0);
 	const [fontsLoaded] = useFonts({
 		Norwester: require('../../assets/fonts/norwester.otf'),
 	});
 	if (!fontsLoaded) {
 		return null;
 	}
-	const renderItem = ({ item }) => {
-		return (
-			<View backgroundColor='#000' height={200}>
-				<Image
-					source={item}
-					width='100%'
-					height={'200'}
-					alt='Carousel Image'
-				/>
-			</View>
-		);
-	};
 	return (
 		<ScrollView width={'100%'} bounces={false}>
-			{carousel.length > 1 && (
-				<>
-					<Carousel
-						loop
-						autoPlay
-						width={width}
-						height={width / 2}
-						data={carousel}
-						renderItem={renderItem}
-						autoPlayInterval={5000}
-						ref={carouselRef}
-					/>
-					{carousel.map((dot, idx) => (
-						<TouchableOpacity
-							key={idx}
-							style={styles.CarouselDots}
-							onPress={() =>
-								setActiveSlide(
-									carouselRef.current?.getCurrentIndex() + 1,
-								)
-							}
-						>
-							<Text color={idx === activeSlide ? 'red' : 'black'}>
-								DOT
-							</Text>
-						</TouchableOpacity>
-					))}
-				</>
-			)}
+			<Carousel data={carousel} />
 			<View style={[styles.Container, style]}>
 				{title && (
 					<Text
@@ -112,5 +67,4 @@ const styles = StyleSheet.create({
 		paddingTop: 32,
 		paddingBottom: 16,
 	},
-	CarouselDots: {},
 });
