3730ab77ace8f7465661e19d08b76a83570f561d
Christopher Jones <tkcwebdev@gmail.com>
2022-10-03 15:43:36 -0700

    23: Template Main: Use new carousel within main template

--- mobile/src/templates/TemplateMain.tsx
+++ mobile/src/templates/TemplateMain.tsx
@@ -1,9 +1,11 @@
-import { ScrollView, Text, View } from 'native-base';
-import { StyleSheet, ViewStyle } from 'react-native';
-import { useFonts } from 'expo-font';
-import { NavigationProp } from '@react-navigation/native';
-import Carousel from '../components/Carousel';
-import testImage from '../../assets/sliders/testImage.png';
+import { ScrollView, Text, View } from "native-base";
+import { StyleSheet, ViewStyle } from "react-native";
+import { useFonts } from "expo-font";
+import { NavigationProp } from "@react-navigation/native";
+import Carousel from "../components/Carousel";
+import testImage from "../../assets/sliders/testImage.png";
+import testImage2 from "../../assets/sliders/testImage2.png";
+import testImage3 from "../../assets/sliders/testImage3.png";
 
 export interface TemplateMainProps {
 	navigation: NavigationProp<any>;
@@ -16,28 +18,28 @@ export interface TemplateMainProps {
 const TemplateMain = ({
 	navigation,
 	children,
-	carousel = [testImage, testImage],
+	carousel = [testImage, testImage2, testImage3],
 	title,
 	style,
 }: TemplateMainProps) => {
 	const [fontsLoaded] = useFonts({
-		Norwester: require('../../assets/fonts/norwester.otf'),
+		Norwester: require("../../assets/fonts/norwester.otf"),
 	});
 	if (!fontsLoaded) {
 		return null;
 	}
 	return (
-		<ScrollView width={'100%'} bounces={false}>
+		<ScrollView width={"100%"} bounces={false}>
 			<Carousel data={carousel} />
 			<View style={[styles.Container, style]}>
 				{title && (
 					<Text
-						fontFamily='Norwester'
-						color='#252525'
+						fontFamily="Norwester"
+						color="#252525"
 						lineHeight={24}
 						fontSize={24}
-						fontWeight='bold'
-						textTransform={'uppercase'}
+						fontWeight="bold"
+						textTransform={"uppercase"}
 						marginBottom={4}
 					>
 						{title}
@@ -54,14 +56,14 @@ export default TemplateMain;
 const styles = StyleSheet.create({
 	page: {
 		flex: 1,
-		backgroundColor: '#252525',
+		backgroundColor: "#252525",
 	},
 	TemplateMain: {
 		flex: 1,
-		backgroundColor: 'white',
+		backgroundColor: "white",
 	},
 	Container: {
-		backgroundColor: 'transparent',
+		backgroundColor: "transparent",
 		flex: 1,
 		paddingHorizontal: 16,
 		paddingTop: 32,
