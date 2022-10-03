781b4fc7865ea01dcdfc1c9e8b49684404f11245
Christopher Jones <tkcwebdev@gmail.com>
2022-10-03 15:34:31 -0700

    23: Carousel: Add new test images

--- mobile/src/components/Carousel.tsx
+++ mobile/src/components/Carousel.tsx
@@ -1,13 +1,14 @@
-import { Image, Text } from 'native-base';
-import { useEffect, useRef, useState } from 'react';
+import { Image, Text } from "native-base";
+import { useEffect, useRef, useState } from "react";
 import {
 	Dimensions,
 	StyleSheet,
 	TouchableOpacity,
 	ViewStyle,
 	View,
-} from 'react-native';
-import ReanimatedCarousel from 'react-native-reanimated-carousel';
+} from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ReanimatedCarousel from "react-native-reanimated-carousel";
 
 export interface ICarouselProps {
 	data?: string[];
@@ -15,52 +16,58 @@ export interface ICarouselProps {
 }
 
 const Carousel = ({ data, style }: ICarouselProps) => {
-	const width = Dimensions.get('window').width;
+	const width = Dimensions.get("window").width;
 	const carouselRef = useRef(null);
 	const [activeSlide, setActiveSlide] = useState(0);
 	const renderItem = ({ item }) => {
 		return (
-			<View style={{ backgroundColor: '#000', height: 200 }}>
+			<View style={{ backgroundColor: "#000", height: 200 }}>
 				<Image
 					source={item}
-					width='100%'
-					height={'200'}
-					alt='Carousel Image'
+					width="100%"
+					height={"200"}
+					alt="Carousel Image"
 				/>
 			</View>
 		);
 	};
 	useEffect(() => {
-		console.log('changing?');
-		console.log('activeSlide', activeSlide);
-		console.log('index', carouselRef.current.getCurrentIndex());
+		console.log("changing?");
+		console.log("activeSlide", activeSlide);
+		console.log("index", carouselRef.current.getCurrentIndex());
 		carouselRef.current.scrollTo(activeSlide);
-	}, [activeSlide]);
+	}, [activeSlide, carouselRef.current.getCurrentIndex()]);
 	return (
 		data.length > 1 && (
 			<View style={style}>
-				<ReanimatedCarousel
-					loop
-					autoPlay
-					width={width}
-					height={width / 2}
-					data={data}
-					renderItem={renderItem}
-					autoPlayInterval={5000}
-					ref={carouselRef}
-					onSnapToItem={() =>
-						setActiveSlide(carouselRef.current.getCurrentIndex())
-					}
-				/>
+				<GestureHandlerRootView>
+					<ReanimatedCarousel
+						loop
+						autoPlay
+						width={width}
+						height={width / 2}
+						data={data}
+						renderItem={renderItem}
+						autoPlayInterval={5000}
+						ref={carouselRef}
+						onSnapToItem={() =>
+							setActiveSlide(
+								carouselRef.current.getCurrentIndex()
+							)
+						}
+					/>
+				</GestureHandlerRootView>
 				<View style={styles.CarouselDots}>
 					{data.map((_dot: any, idx: number) => (
 						<TouchableOpacity
 							key={idx}
 							onPress={() => {
-								setActiveSlide(
-									carouselRef.current.getCurrentIndex(),
-								);
-								carouselRef.current.scrollTo(idx);
+								console.log("idx", idx);
+								setActiveSlide(idx);
+								carouselRef.current.scrollTo({
+									index: idx,
+									animated: true,
+								});
 							}}
 						>
 							<View
@@ -82,25 +89,27 @@ const styles = StyleSheet.create({
 	Carousel: {},
 	CarouselDots: {
 		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		marginTop: -32,
+		flexDirection: "row",
+		justifyContent: "center",
+		marginTop: -48,
 		flexGap: 8,
 	},
 	CarouselDot: {
-		backgroundColor: 'white',
+		backgroundColor: "white",
 		height: 20,
 		width: 20,
 		borderRadius: 20,
-		position: 'relative',
+		position: "relative",
 		opacity: 0.5,
+		marginHorizontal: 2,
 	},
 	CarouselActiveDot: {
-		backgroundColor: 'white',
+		backgroundColor: "white",
 		height: 20,
 		width: 20,
 		borderRadius: 20,
 		opacity: 0.9,
+		marginHorizontal: 2,
 	},
 });
 
