fe36f75081928b61d07efaf0dd755e6335a1e9f5
Christopher Jones <tkcwebdev@gmail.com>
2022-10-25 01:27:37 -0700

    33: Remove console log from check regex helper function

--- mobile/src/utils/helper.ts
+++ mobile/src/utils/helper.ts
@@ -1,6 +1,5 @@
 export const checkRegex = (value: string, expression: string): boolean => {
 	const regex = new RegExp(expression);
 	const isValid = regex.test(value);
-	console.log('isValid Email', isValid);
 	return isValid;
 };
