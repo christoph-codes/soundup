914313d3c44dca4a707c85222c56ce718f99d82a
Christopher Jones <tkcwebdev@gmail.com>
2022-10-25 01:39:11 -0700

    33: Write documentation for the auth provider

--- mobile/src/providers/AuthProvider.tsx
+++ mobile/src/providers/AuthProvider.tsx
@@ -24,16 +24,23 @@ import {
 import { NavigationProp } from '@react-navigation/native';
 
 export interface IAuthContext {
+	/* The user object that will be used in the AuthContext. */
 	user: {
 		type?: 'default' | 'paid';
 		name?: string;
 		email?: string;
 		authId?: string;
 	};
+	/* A boolean that is used to determine if the user is loading or not. */
 	loading: boolean;
+	/* A boolean that determines if there was an error regarding authentication. */
 	error?: string;
+	/* A function that takes in an email and password, and if they are not empty, it will sign in the
+	user with the email and password */
 	login: (email: string, password: string) => void;
+	/* A function that takes in a navigation prop to redirect the user and logs the user out. */
 	logout: (navigation: NavigationProp<any>) => void;
+	/* Creating a new user function with the given email and password, then adding the user to the database. */
 	createAccountWithEmailAndPassword: (
 		name: string,
 		email: string,
@@ -60,6 +67,11 @@ export const DefaultContext: IAuthContext = {
 
 const AuthContext = createContext<IAuthContext>(DefaultContext);
 
+/**
+ * Provides user state to the entire application
+ * @param {IAuthProviderProps}  - IAuthProviderProps
+ * @returns The AuthProvider is being returned.
+ */
 const AuthProvider = ({ children }: IAuthProviderProps) => {
 	const [user, setUser] = useState<IAuthContext['user']>(
 		DefaultContext['user'],
@@ -68,6 +80,11 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
 	const [error, setError] = useState<IAuthContext['error']>('');
 	console.log('user:', user);
 
+	/**
+	 * It takes a user id, looks up the user in the database, and returns the user if it exists
+	 * @param {string} uid - The user's unique ID from the authentication provider.
+	 * @returns The user object is being returned.
+	 */
 	const lookupUser = async (uid: string): Promise<IAuthContext['user']> => {
 		const userRef = collection(db, 'users');
 		const storedUser = query(userRef, where('authId', '==', uid));
@@ -84,7 +101,8 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
 		}
 	};
 
-	// Listen for authentication state to change.
+	/* Checking to see if the user is authenticated. If they are, it will set the user to the
+	authenticated user. If not, it will set the error to the error message. */
 	useEffect(() => {
 		onAuthStateChanged(auth, async (fireUser) => {
 			if (fireUser != null) {
@@ -103,13 +121,18 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
 		});
 	}, []);
 
+	/**
+	 * The login function takes in an email and password, and if they are not empty, it will sign in the
+	 * user with the email and password
+	 * @param {string} email - string - The email address of the user
+	 * @param {string} password - string - The password of the user
+	 */
 	const login = (email: string, password: string) => {
 		if (!email || !password) {
 			throw new Error(
 				'You must complete all fields to properly access your account',
 			);
 		}
-		console.log('logging in user');
 		// TODO: Add login function with firebase for username and password
 		signInWithEmailAndPassword(auth, email, password).then(async (data) => {
 			if (data.user) {
@@ -120,6 +143,10 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
 			}
 		});
 	};
+	/**
+	 * It logs the user out and navigates to the sign in screen.
+	 * @param navigation - NavigationProp<any>
+	 */
 	const logout = (navigation: NavigationProp<any>) => {
 		signOut(auth)
 			.then(() => {
@@ -130,6 +157,12 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
 				throw new Error(err);
 			});
 	};
+	/**
+	 * It creates an auth user with the given email and password, then adds the user to the database
+	 * @param {string} name - string,
+	 * @param {string} email - string,
+	 * @param {string} password - string
+	 */
 	const createAccountWithEmailAndPassword = async (
 		name: string,
 		email: string,
@@ -178,6 +211,9 @@ const AuthProvider = ({ children }: IAuthProviderProps) => {
 	);
 };
 
+/**
+ * It returns the value of the AuthContext Provider
+ */
 export const useAuth = () => useContext(AuthContext);
 
 export default AuthProvider;
