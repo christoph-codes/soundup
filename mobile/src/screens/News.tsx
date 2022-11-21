84eeeb69101e40cfb9593ad9afce544561bc7abb
Christopher Jones <tkcwebdev@gmail.com>
2022-11-20 23:23:27 -0800

    main: Update news feed to pull from contentful

--- mobile/src/screens/News.tsx
+++ mobile/src/screens/News.tsx
@@ -1,8 +1,23 @@
-import { Text } from 'native-base';
-import { StyleSheet } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import NewsArticle from '../components/NewsArticle';
 import TemplateMain from '../templates/TemplateMain';
 
 const News = ({ navigation }) => {
+	const [articles, setArticles] = useState<any>({});
+	// TODO: Turn this fetch into a reduce that builds the object the proper way instead of breaking out the image and the content
+	// TODO: Turn into Provider to fetch once per app session
+	useEffect(() => {
+		axios
+			.get(
+				`https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&include=1`,
+			)
+			.then((res) => {
+				setArticles(res.data);
+			})
+			.catch((err) => console.log);
+	}, []);
 	return (
 		<TemplateMain
 			style={styles.News}
@@ -10,7 +25,21 @@ const News = ({ navigation }) => {
 			navigation={navigation}
 			carousel
 		>
-			<Text>News content goes here</Text>
+			<View>
+				{articles?.items?.map((article, index) => {
+					return (
+						<NewsArticle
+							navigation={navigation}
+							image={
+								articles['includes']?.Asset[index]?.fields?.file
+									?.url
+							}
+							article={article}
+							key={index}
+						/>
+					);
+				})}
+			</View>
 		</TemplateMain>
 	);
 };
@@ -19,7 +48,6 @@ export default News;
 
 const styles = StyleSheet.create({
 	News: {
-		backgroundColor: 'red',
 		flex: 1,
 	},
 });
