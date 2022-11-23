14cd8b46ab290fff72a885a24c3dcc868269b144
Christopher Jones <tkcwebdev@gmail.com>
2022-11-23 13:15:55 -0800

    41: Use Featured posts from provider

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -3,36 +3,28 @@ import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import NewsArticle from '../components/NewsArticle';
 import VideoArticle, { IVideoArticleContent } from '../components/VideoArticle';
+import { useArticles } from '../providers/ArticleProvider';
 import { useAuth } from '../providers/AuthProvider';
 import TemplateMain from '../templates/TemplateMain';
-import contentful from '../utils/contentful';
 
 const Home = ({ navigation }) => {
 	const { user } = useAuth();
-	const [posts, setPosts] = useState<any>({});
+	const { posts, featuredPosts } = useArticles();
 	const testVideoArticle: IVideoArticleContent = {
 		title: 'Test article',
 		postedDate: Date.now(),
 		link: 'Home',
 		videoUrl: 'NmaM5VsZCJM',
 	};
-	useEffect(() => {
-		contentful
-			.get('')
-			.then((res) => {
-				setPosts(res.data);
-			})
-			.catch(console.log);
-	}, []);
 	return (
 		<TemplateMain
 			style={styles.Home}
 			navigation={navigation}
 			title={user?.name ? `Hey ${user.name}!` : 'Latest Updates'}
-			carousel
+			carousel={featuredPosts}
 		>
 			<VideoArticle navigation={navigation} article={testVideoArticle} />
-			{posts.items?.map((post, index) => {
+			{posts?.items?.map((post, index) => {
 				if (post.sys.contentType.sys.id === 'newsArticle') {
 					return (
 						<NewsArticle
