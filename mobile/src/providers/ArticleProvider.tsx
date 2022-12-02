2bbd013c45af2ed9b89225c3bd6d434a1c2798b9
Christopher Jones <tkcwebdev@gmail.com>
2022-12-02 09:57:34 -0800

    43: Divided articles, featured posts, and ads to be displayed throughout the articles provider

--- mobile/src/providers/ArticleProvider.tsx
+++ mobile/src/providers/ArticleProvider.tsx
@@ -1,43 +1,83 @@
+import axios from 'axios';
 import { createContext, useContext, useEffect, useState } from 'react';
+import { IAdProps } from '../components/Ad';
 import { ICarouselProps } from '../components/Carousel';
 import contentful from '../utils/contentful';
+import { getImage } from '../utils/helper';
 
 const ArticleContext = createContext(null);
 
 const ArticleProvider = ({ children }) => {
-	const [posts, setPosts] = useState<any>({});
+	const [posts, setPosts] = useState<any>(null);
 	const [featuredPosts, setFeaturedPosts] = useState<ICarouselProps['data']>(
 		[],
 	);
+	const [articles, setArticles] = useState<ICarouselProps['data']>([]);
+	const [ads, setAds] = useState<IAdProps[]>([]);
 	useEffect(() => {
-		contentful
-			.get('')
-			.then((res) => {
-				setPosts(res.data);
-			})
-			.catch(console.log);
+		if (posts === null) {
+			contentful('all')
+				.get('')
+				.then((res) => {
+					setPosts(res.data);
+				})
+				.catch(console.log);
+		}
 	}, []);
-	/* Filtering the posts to only show the featured posts. */
 
+	/* Filtering the posts to only show the featured posts. */
 	useEffect(() => {
 		// FIX: fires twice on initial render and update
-		const featured = posts?.items?.reduce((acc, cv, idx) => {
-			if (cv?.fields?.featured) {
-				return [
-					...acc,
-					{
-						title: cv?.fields?.title,
-						image: posts['includes']?.Asset[idx]?.fields?.file?.url,
-						article: cv,
-					},
-				];
+		posts?.items?.forEach(async (cv, idx) => {
+			const contentImage = await getImage(
+				cv?.fields.featuredImage.sys.id,
+			);
+
+			if (cv.sys.contentType.sys.id === 'newsArticle') {
+				if (cv?.fields?.featured) {
+					console.log('cv?.fields', cv?.fields.featuredImage.sys.id);
+					setFeaturedPosts((prev) => {
+						return [
+							...prev,
+							{
+								title: cv?.fields?.title,
+								image: contentImage,
+								article: cv,
+							},
+						];
+					});
+				} else {
+					setArticles((prev) => {
+						return [
+							...prev,
+							{
+								title: cv?.fields?.title,
+								image: posts['includes']?.Asset[idx]?.fields
+									?.file?.url,
+								article: cv,
+							},
+						];
+					});
+				}
 			}
-			return acc;
-		}, []); // END Reduce function not dependency array
-		() => setFeaturedPosts(featured);
+			if (cv.sys.contentType.sys.id === 'ads') {
+				setAds((prev) => {
+					return [
+						...prev,
+						{
+							image: posts['includes']?.Asset[idx]?.fields?.file
+								?.url,
+							url: cv.fields.url,
+							name: cv.fields.adName,
+							company: cv.fields.company,
+						},
+					];
+				});
+			}
+		});
 	}, [posts]);
 	return (
-		<ArticleContext.Provider value={{ posts, featuredPosts }}>
+		<ArticleContext.Provider value={{ featuredPosts, articles, ads }}>
 			{children}
 		</ArticleContext.Provider>
 	);
