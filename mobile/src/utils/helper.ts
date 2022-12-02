b45a5712959bdde0321bccc904651833d3c352fb
Christopher Jones <tkcwebdev@gmail.com>
2022-12-02 09:58:41 -0800

    43: Image getter helper function. WIP

--- mobile/src/utils/helper.ts
+++ mobile/src/utils/helper.ts
@@ -1,5 +1,17 @@
+import axios from "axios";
+
 export const checkRegex = (value: string, expression: string): boolean => {
 	const regex = new RegExp(expression);
 	const isValid = regex.test(value);
 	return isValid;
 };
+
+export const getImage = async (assetId: string) => {
+	return await axios
+		.get(`https://images.ctfassets.net/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/assets/${assetId}`)
+		.then(res => {
+			console.log('res.data',res.data.fields);
+			return res.data.fields.file.url
+		})
+		.catch(console.log);
+}
