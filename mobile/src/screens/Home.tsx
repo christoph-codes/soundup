92a2b3a7f9035a6c2fe62bfb8bcb924d68ca7722
Christopher Jones <tkcwebdev@gmail.com>
2022-12-26 20:02:55 -0800

    43: Create new feed for the home screen and conditionally render the components based on the article type

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,19 +1,34 @@
+import { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
+import Ad from '../components/Ad';
 import Modal from '../components/Modal';
 import NewsArticle from '../components/NewsArticle';
-import VideoArticle, { IVideoArticleContent } from '../components/VideoArticle';
+import VideoArticle, { IVideoProps } from '../components/VideoArticle';
 import { useArticles } from '../providers/ArticleProvider';
 import { useAuth } from '../providers/AuthProvider';
 import TemplateMain from '../templates/TemplateMain';
 
 const Home = ({ navigation }) => {
 	const { user } = useAuth();
-	const { articles, featuredPosts } = useArticles();
-	const testVideoArticle: IVideoArticleContent = {
-		title: 'Test article',
-		postedDate: Date.now(),
-		link: 'Home',
-		videoUrl: 'NmaM5VsZCJM',
+	const { articles, featuredPosts, ads } = useArticles();
+
+	/**
+	 * It takes an array of articles and ads, and returns a new array of articles and ads, with ads
+	 * inserted every 4th article
+	 * @returns An array of articles and ads.
+	 */
+	const feed = () => {
+		let adNum = 0;
+		const allArticles = articles.reduce((acc, cv, idx) => {
+			if (idx > 1 && idx % 4 === 0) {
+				acc.push(ads[adNum]);
+				adNum++;
+			} else {
+				acc.push(cv);
+			}
+			return acc;
+		}, []);
+		return allArticles;
 	};
 	return (
 		<TemplateMain
@@ -22,25 +37,37 @@ const Home = ({ navigation }) => {
 			title={user?.name ? `Hey ${user.name}!` : 'Latest Updates'}
 			carousel={featuredPosts}
 		>
-			<VideoArticle navigation={navigation} article={testVideoArticle} />
-			{articles?.map((post, index) => {
-				return (
-					<NewsArticle
-						key={index}
-						image={post.image}
-						article={post.article}
-						navigation={navigation}
-					/>
-				);
+			{/* <VideoArticle navigation={navigation} article={testVideoArticle} /> */}
+			{feed()?.map((post, index) => {
+				if (post?.article?.youtubeId) {
+					return (
+						<VideoArticle
+							key={index}
+							article={post.article}
+							navigation={navigation}
+						/>
+					);
+				}
+				if (post?.article?.adName && user.email) {
+					return (
+						<Ad
+							key={index}
+							image={post?.image}
+							article={post?.article}
+						/>
+					);
+				}
+				if (post?.article?.content && post.image) {
+					return (
+						<NewsArticle
+							key={index}
+							image={post?.image}
+							article={post.article}
+							navigation={navigation}
+						/>
+					);
+				}
 			})}
-			{/** TODO: Dont forget to delete */}
-			<Modal
-				buttonLabel='Open This Thang'
-				title='This is the title'
-				message='This is the messag to display'
-				successLabel='Good To Go'
-				successCallback={() => console.log('YERP!')}
-			/>
 		</TemplateMain>
 	);
 };
