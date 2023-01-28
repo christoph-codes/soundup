4be67f8b6ee68b0c86aa7923d637ffebf06b1647
Christopher Jones <tkcwebdev@gmail.com>
2023-01-28 08:41:30 -0800

    bugfixes: Add limit and skip for content reload for updated fetches. WIP

--- mobile/src/utils/contentful.ts
+++ mobile/src/utils/contentful.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 type TFetchOptions = 'featured' | 'articles' | 'videos' | 'images' | 'all'
 
-const contentful = (fetchOption: TFetchOptions) => {
+const contentful = (fetchOption: TFetchOptions, paginationLimit: number, skip: number = paginationLimit ) => {
     const contentfulContentType = () => {
         switch (fetchOption) {
             case 'featured':
@@ -14,7 +14,7 @@ const contentful = (fetchOption: TFetchOptions) => {
             case 'images':
                 return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/assets?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`
             case 'all':
-                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`
+                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&limit=${paginationLimit}&skip=${skip}`
         }
     }
     return axios.create({
