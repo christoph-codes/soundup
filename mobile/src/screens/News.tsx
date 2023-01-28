810b524ec3d9c1188bc240a74d3c6b4e176d24cc
Christopher Jones <tkcwebdev@gmail.com>
2023-01-28 08:42:25 -0800

    bugfixes: Add news page updates with new content structure. WIP

--- mobile/src/screens/News.tsx
+++ mobile/src/screens/News.tsx
@@ -1,43 +1,56 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import Ad from '../components/Ad';
 import NewsArticle from '../components/NewsArticle';
+import { useArticles } from '../providers/ArticleProvider';
+import { useAuth } from '../providers/AuthProvider';
 import TemplateMain from '../templates/TemplateMain';
 
 const News = ({ navigation }) => {
-	const [articles, setArticles] = useState<any>({});
-	// TODO: Turn this fetch into a reduce that builds the object the proper way instead of breaking out the image and the content
-	// TODO: Turn into Provider to fetch once per app session
-	useEffect(() => {
-		axios
-			.get(
-				`https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&include=1`,
-			)
-			.then((res) => {
-				setArticles(res.data);
-			})
-			.catch((err) => console.log);
-	}, []);
+	const { user } = useAuth();
+	const { articles, ads } = useArticles();
+	const feed = () => {
+		let adNum = 0;
+		const allArticles = articles.reduce((acc, cv, idx) => {
+			console.log('article:', cv.article.title);
+			if (idx > 2 && idx % 4 === 0) {
+				acc[idx] = ads[adNum];
+				adNum++;
+			} else {
+				acc[idx] = cv;
+			}
+			return acc;
+		}, []);
+		return allArticles;
+	};
 	return (
 		<TemplateMain
 			style={styles.News}
 			title='News'
 			navigation={navigation}
 			carousel
+			onRefresh={() => console.log('Refreshing')}
 		>
 			<View>
-				{articles?.items?.map((article, index) => {
-					return (
-						<NewsArticle
-							navigation={navigation}
-							image={
-								articles['includes']?.Asset[index]?.fields?.file
-									?.url
-							}
-							article={article}
-							key={index}
-						/>
-					);
+				{feed()?.map((post, index) => {
+					if (post?.type === 'ads' && !user.email) {
+						return (
+							<Ad
+								key={index}
+								image={post?.image}
+								article={post?.article}
+							/>
+						);
+					}
+					if (post?.type === 'newsArticle' && post.image) {
+						return (
+							<NewsArticle
+								key={index}
+								image={post?.image}
+								article={post.article}
+								navigation={navigation}
+							/>
+						);
+					}
 				})}
 			</View>
 		</TemplateMain>
