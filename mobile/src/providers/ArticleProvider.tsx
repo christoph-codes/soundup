d07ca65535bf7e25927ba01780c0829a3c8cb2ce
Christopher Jones <tkcwebdev@gmail.com>
2023-01-28 17:36:11 -0800

    bugfixes: Add reducer to main provider, WIP

--- mobile/src/providers/ArticleProvider.tsx
+++ mobile/src/providers/ArticleProvider.tsx
@@ -1,35 +1,39 @@
-import {
-	createContext,
-	useContext,
-	useEffect,
-	useState,
-	useCallback,
-} from 'react';
-import { IAdProps } from '../components/Ad';
-import { ICarouselProps } from '../components/Carousel';
-import { IVideoProps } from '../components/VideoArticle';
+import { createContext, useContext, useState, useReducer } from 'react';
+import contentReducer from '../reducers/contentReducer';
 import contentful from '../utils/contentful';
 import { getImage } from '../utils/helper';
 
-const ArticleContext = createContext(null);
+const ContentContext = createContext(null);
 
-const ArticleProvider = ({ children }) => {
-	const fetchLimit: number = 10;
-	const [posts, setPosts] = useState<any>(null);
+const initialState = {
+	articles: [],
+	featured: [],
+	ads: [],
+	videos: [],
+};
+
+const ContentProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(contentReducer, initialState);
 	const [pagination, setPagination] = useState<number>(1);
-	const [featuredPosts, setFeaturedPosts] = useState<ICarouselProps['data']>(
-		[],
-	);
-	const [articles, setArticles] = useState<ICarouselProps['data']>([]);
-	const [ads, setAds] = useState<IAdProps[]>([]);
-	const [videos, setVideos] = useState<IVideoProps[]>([]);
 
-	const getArticles = useCallback(() => {
+	/**
+	 * A function that queries Contentful for a number of articles with refetch capabilities
+	 * @param fetchLimit = The number of articles to fetch from contentful
+	 * @param pagination The page number you are fetching for
+	 */
+	const getArticles = (fetchLimit = 5, pagination = 1) => {
 		console.log('getting articles');
 		contentful('all', fetchLimit, fetchLimit * pagination)
 			.get('')
 			.then((res) => {
-				const structuredPosts = res.data.items?.forEach(async (cv) => {
+				const structuredPosts: {
+					featured: boolean;
+					image: string;
+					article: any;
+					type: 'newsArticle' | 'videoArticle' | 'ads';
+				}[] = res.data.items.map(async (cv, idx) => {
+					console.log('cv', cv);
+					// TODO: Convert to switch
 					const contentImage =
 						cv?.sys?.contentType?.sys?.id === 'newsArticle'
 							? await getImage(cv?.fields?.featuredImage?.sys?.id)
@@ -39,28 +43,29 @@ const ArticleProvider = ({ children }) => {
 							? `//i3.ytimg.com/vi/${cv.fields.youtubeId}/maxresdefault.jpg`
 							: null;
 					if (contentImage !== null) {
-						setArticles((prev) => {
-							return [
-								...prev,
-								{
-									featured: cv.fields.featured,
-									image: contentImage,
-									article: cv.fields,
-									type: cv?.sys?.contentType?.sys?.id,
-								},
-							];
+						console.log('idx', {
+							key: idx,
+							featured: cv.fields.featured,
+							image: contentImage,
+							article: cv.fields,
+							type: cv?.sys?.contentType?.sys?.id,
 						});
+						return {
+							key: idx,
+							featured: cv.fields.featured,
+							image: contentImage,
+							article: cv.fields,
+							type: cv?.sys?.contentType?.sys?.id,
+						};
 					}
 				});
-				setPosts(structuredPosts);
-				setPagination(pagination);
+				console.log('struc', structuredPosts);
+				dispatch({
+					articles: structuredPosts,
+				});
 			})
 			.catch((err) => console.log('Error!!!!', err));
-	}, [pagination]);
-
-	useEffect(() => {
-		getArticles();
-	}, [pagination]);
+	};
 
 	const loadMoreArticles = () => {
 		console.log('pagination', pagination);
@@ -69,14 +74,18 @@ const ArticleProvider = ({ children }) => {
 	};
 
 	return (
-		<ArticleContext.Provider
-			value={{ featuredPosts, articles, ads, videos, loadMoreArticles }}
+		<ContentContext.Provider
+			value={{
+				state,
+				getArticles,
+				loadMoreArticles,
+			}}
 		>
 			{children}
-		</ArticleContext.Provider>
+		</ContentContext.Provider>
 	);
 };
 
-export const useArticles = () => useContext(ArticleContext);
+export const useArticles = () => useContext(ContentContext);
 
-export default ArticleProvider;
+export default ContentProvider;
