77e6de85f510e9625def8e49994da4851ed595b5
christoph-codes <tkcwebdev@gmail.com>
2023-01-29 23:45:21 -0800

    bugfixes: Create feed component to add ads when user is not logged in

---
+++ mobile/src/components/Feed.tsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+import { useContent } from '../providers/ArticleProvider';
+import { useAuth } from '../providers/AuthProvider';
+import Ad from './Ad';
+import NewsArticle from './NewsArticle';
+import VideoArticle from './VideoArticle';
+
+const Feed = ({ navigation, arrayOfArticles }) => {
+	const { user } = useAuth();
+	const { state, getContent } = useContent();
+
+	useEffect(() => {
+		getContent('ads', state.ads.pagination);
+	}, [state.ads.pagination]);
+
+	const feed = () => {
+		let adNum = 0;
+		const articlesWithAds = arrayOfArticles.reduce((acc, cv, idx) => {
+			if (idx > 2 && idx % 4 === 0) {
+				acc[idx] = state.ads.data[adNum];
+				adNum++;
+			} else {
+				acc[idx] = cv;
+			}
+			return acc;
+		}, []);
+		return articlesWithAds;
+	};
+
+	return (
+		<>
+			{feed()?.map((post, index) => {
+				// console.log('TYPE', post?.type);
+				if (post?.type === 'videoArticle') {
+					return (
+						<VideoArticle
+							key={index}
+							article={post.article}
+							navigation={navigation}
+						/>
+					);
+				}
+				if (post?.type === 'ads' && !user.email) {
+					return (
+						<Ad
+							key={index}
+							image={post?.image}
+							article={post?.article}
+						/>
+					);
+				}
+				if (post?.type === 'newsArticle' && post.image) {
+					return (
+						<NewsArticle
+							key={index}
+							image={post?.image}
+							article={post.article}
+							navigation={navigation}
+						/>
+					);
+				}
+			})}
+		</>
+	);
+};
+
+export default Feed;
