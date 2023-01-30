8a0441a22d217a5b01860aa7759960c009dbef59
christoph-codes <tkcwebdev@gmail.com>
2023-01-29 23:46:41 -0800

    bugfixes: Update contentful fetches to be more dynamic for the different types of content

--- mobile/src/utils/contentful.ts
+++ mobile/src/utils/contentful.ts
@@ -1,25 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 
-type TFetchOptions = 'featured' | 'articles' | 'videos' | 'images' | 'all'
+export type TFetchOptions =
+	| 'featured'
+	| 'articles'
+	| 'videos'
+	| 'ads'
+	| 'images'
+	| 'all';
 
-const contentful = (fetchOption: TFetchOptions, paginationLimit: number, skip: number = paginationLimit ) => {
-    const contentfulContentType = () => {
-        switch (fetchOption) {
-            case 'featured':
-                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&content_type=newsArticle&fields.featured=true`
-            case 'articles':
-                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&content_type=newsArticle`
-            case 'videos':
-                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&content_type=videoArticle`
-            case 'images':
-                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/assets?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`
-            case 'all':
-                return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&limit=${paginationLimit}&skip=${skip}`
-        }
-    }
-    return axios.create({
-            baseURL: contentfulContentType(),
-    });
-}
+const contentful = (
+	fetchOption: TFetchOptions,
+	paginationLimit: number,
+	skip: number = paginationLimit,
+) => {
+	const contentfulContentType = () => {
+		switch (fetchOption) {
+			case 'featured':
+				return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&fields.featured=true&limit=${paginationLimit}&skip=${skip}`;
+			case 'articles':
+				return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&content_type=newsArticle&limit=${paginationLimit}&skip=${skip}`;
+			case 'videos':
+				return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&content_type=videoArticle&limit=${paginationLimit}&skip=${skip}`;
+			case 'ads':
+				return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&content_type=ads&limit=${paginationLimit}&skip=${skip}`;
+			case 'images':
+				return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/assets?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`;
+			case 'all':
+				return `https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}&limit=${paginationLimit}&skip=${skip}`;
+		}
+	};
+	return axios.create({
+		baseURL: contentfulContentType(),
+	});
+};
 
 export default contentful;
--- mobile/src/utils/helper.ts
+++ mobile/src/utils/helper.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const checkRegex = (value: string, expression: string): boolean => {
 	const regex = new RegExp(expression);
@@ -8,7 +8,9 @@ export const checkRegex = (value: string, expression: string): boolean => {
 
 export const getImage = async (assetId: string) => {
 	return await axios
-		.get(`https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/assets/${assetId}?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`)
-		.then(res => res.data.fields.file.url)
-		.catch(err => console.log('Image Fetch ERROR:', err));
-}
+		.get(
+			`https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/${process.env.REACT_APP_CONTENTFUL_ENVIRONMENT}/assets/${assetId}?access_token=${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`,
+		)
+		.then((res) => res.data.fields.file.url)
+		.catch((err) => console.log('Image Fetch ERROR:', err));
+};
