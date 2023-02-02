e0fb2b4e05c861b20cc57b08d8448bf684444381
christoph-codes <tkcwebdev@gmail.com>
2023-02-01 23:47:49 -0800

    bugfixes: Create news page with feature pulling from provider. Resolves issue #8

--- mobile/src/screens/News.tsx
+++ mobile/src/screens/News.tsx
@@ -1,32 +1,23 @@
-import { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Feed from '../components/Feed';
 import { useContent } from '../providers/ArticleProvider';
 import TemplateMain from '../templates/TemplateMain';
 
 const News = ({ navigation }) => {
-	const { state, getContent, incrementPagination } = useContent();
-
-	useEffect(() => {
-		getContent('articles', state.articles.pagination);
-	}, [state.articles.pagination]);
-
-	console.log('art', state.articles.data);
+	const { state, getContent } = useContent();
 
 	return (
 		<TemplateMain
 			style={styles.News}
 			title='News'
 			navigation={navigation}
-			carousel={state.articles.data.filter((art) => art.article.featured)}
+			carousel={state.articles.data.filter(
+				(post) => post.article.featured,
+			)}
 			onRefresh={() => getContent('articles')}
-			onEndReach={() => incrementPagination('articles')}
 		>
 			<View>
-				<Feed
-					navigation={navigation}
-					arrayOfArticles={state.articles.data}
-				/>
+				<Feed navigation={navigation} fetchOption='articles' />
 			</View>
 		</TemplateMain>
 	);
