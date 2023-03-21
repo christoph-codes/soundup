33d4a9b003a44499a3781dbcaab8a6f4622ff36f
Christopher Jones <tkcwebdev@gmail.com>
2023-03-20 22:36:54 -0700

    main: Create new Article component for consistency for content

---
+++ mobile/src/components/Article.tsx
@@ -0,0 +1,26 @@
+import { HStack } from 'native-base';
+import { StyleSheet } from 'react-native';
+
+const Article = ({ children, ...rest }) => {
+	return (
+		<HStack
+			style={styles.Article}
+			flexBasis={{ base: '100%', md: '49%' }}
+			{...rest}
+		>
+			{children}
+		</HStack>
+	);
+};
+
+export default Article;
+
+const styles = StyleSheet.create({
+	Article: {
+		paddingBottom: 16,
+		marginBottom: 16,
+		borderStyleBottom: 'solid',
+		borderBottomColor: '#F1F1F1',
+		borderBottomWidth: 1,
+	},
+});
