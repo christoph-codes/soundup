a133764ced5d17030240cf8801e4879fad928b48
Christopher Jones <tkcwebdev@gmail.com>
2023-06-29 11:48:29 -0700

    54: Add getContent to helper file

--- mobile/src/utils/helper.ts
+++ mobile/src/utils/helper.ts
@@ -1,4 +1,11 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
+import contentful, { TFetchOptions } from './contentful';
+
+export const log = (message: string, value?: any) => {
+	const osPrefix = Platform.OS === 'ios' ? 'iOS' : 'Android';
+	console.log(`[${osPrefix}] ${message}`, value || '');
+  };
 
 export const checkRegex = (value: string, expression: string): boolean => {
 	const regex = new RegExp(expression);
@@ -17,11 +24,64 @@ export const getImage = async (assetId: string): Promise<string> => {
 		.then((res) => res.data.fields.file.url)
 		.catch((err) => {
 			if(axios.isCancel(err)) {
-				console.log('Image Fetch ERROR:', err.message)
+				log('Image Fetch ERROR:', err.message)
 			} else if (err.code === 'ECONNABTED') {
-				console.log('Request timed out:', err.message);
+				log('Request timed out:', err.message);
 			  } else {
-				console.error(err);
+				console.error('getImage', err);
 			  }
 		});
+};
+
+
+export const getContent = async (fetchOption: TFetchOptions = 'all', pagination = 0) => {
+
+		const fetchLimit = 50;
+		return contentful(fetchOption, fetchLimit, fetchLimit * pagination)
+			.get('')
+			.then((res) => {
+				const promises = res.data.items.map(async (cv, idx) => {
+					const contentImage = async () => {
+						switch (cv.sys.contentType.sys.id) {
+							case 'newsArticle':
+								const articleImage = getImage(
+									cv.fields.featuredImage.sys.id,
+								);
+								return await articleImage;
+							case 'ads':
+								const image = getImage(
+									cv.fields.artwork.sys.id,
+								);
+								return await image;
+							case 'videoArticle':
+								return `//i3.ytimg.com/vi/${cv.fields.youtubeId}/hqdefault.jpg`;
+							default:
+								return null;
+						}
+					};
+					const imageVariable =
+						(await contentImage()) !== null &&
+						(await contentImage());
+
+					if (imageVariable !== null) {
+						const contentType =
+							cv.sys.contentType.sys.id === 'ads'
+								? 'ads'
+								: cv.sys.contentType.sys.id === 'videoArticle'
+								? 'videos'
+								: 'newsArticle';
+						const newStructure = {
+							image: imageVariable,
+							article: cv.fields,
+							type: contentType,
+							featured: cv.fields.featured,
+							publishDate: cv.sys.createdAt,
+						};
+						return newStructure;
+					}
+				});
+				return Promise.all(promises).then((resolved) => resolved);
+			})
+			.catch((err) => log('Error!!!!', err))
+			// .finally(() => log('done'));
 };
