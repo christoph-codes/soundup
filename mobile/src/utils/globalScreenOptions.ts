cfd678426ff08c1b542a75d5ec85d4f9cd07d476
christoph-codes <tkcwebdev@gmail.com>
2023-07-03 14:11:28 -0700

    60: Create global objects for repurposing throughout app. Screen options and navigation only prop type for screens that require it

---
+++ mobile/src/utils/globalScreenOptions.ts
@@ -0,0 +1,12 @@
+export const globalScreenOptions: any = {
+	headerShown: false,
+	gestureEnabled: false,
+};
+
+export const headerOptions: any = {
+	headerStyle: {
+		backgroundColor: '#252525',
+		border: 0,
+	},
+	headerTintColor: '#fff',
+};
