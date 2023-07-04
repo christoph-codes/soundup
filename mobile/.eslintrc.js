94a4889f11b6bdd5bf96f7c6ba5a3bb5feee5fd2
christoph-codes <tkcwebdev@gmail.com>
2023-07-04 13:47:22 -0700

    58: Linting clean up, no useless fragements and one js expression per line rule ignores

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -44,5 +44,7 @@ module.exports = {
 		'react/destructuring-assignment': 0,
 		'react/no-unstable-nested-components': 0,
 		'react/jsx-props-no-spreading': 0,
+		'react/jsx-no-useless-fragment': 0,
+		'react/jsx-one-expression-per-line': 0,
 	},
 };
